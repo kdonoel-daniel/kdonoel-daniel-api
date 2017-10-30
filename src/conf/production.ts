def4a5990d6869a623a9b5247c69c9e5c748d604
Benjamin DANIEL <BenjDaniel90@gmail.com>
2017-10-30 14:43:31 +0100

    Update production configuration

--- src/conf/production.ts
+++ src/conf/production.ts
@@ -1,1 +1,15 @@
-export default {};
+const conf: Conf = {
+	log: {
+		level: 'info',
+		http: 'dev'
+	},
+	mongo: {
+		url: 'mongodb://127.0.0.1:27017/kdos'
+	},
+	jwt: {
+		secret: 'secret_key',
+		expiresIn: '30 days'
+	},
+};
+
+export default conf;
